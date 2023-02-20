9f3c27b284369fa2f7090ad6ef399b51dd8d7237
Ethan <ethantam@berkeley.edu>
2023-02-20 00:32:03 -0800

    finished 2

---
+++ Assignment1/ReverseVowels.py
@@ -0,0 +1,38 @@
+# Forward-Backward Two-Pointer
+
+def reverseVowels(string):
+    front = ""
+    end = ""
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    p1 = 0
+    p2 = len(string) - 1
+    while p1 != p2 and p2 > p1:
+        if string[p1] not in vowels and string[p2] not in vowels:
+            front += string[p1]
+            end = string[p2] + end
+            p1 += 1
+            p2 -= 1
+        else:
+            if string[p1] in vowels and string[p2] in vowels:
+                front += string[p2]
+                end = string[p1] + end
+                p1 += 1
+                p2 -= 1
+            elif string[p1] in vowels:
+                end = string[p2] + end
+                p2 -= 1
+            elif string[p2] in vowels:
+                front += string[p1]
+                p1 += 1
+    if p1 == p2:
+        front += string[p1]
+    return front + end
+
+print(reverseVowels("Uber Career Prep"))
+print(reverseVowels("xyz"))
+print(reverseVowels("flamingo"))
+print(reverseVowels(""))
+print(reverseVowels("oe"))
+print(reverseVowels("ota"))
+
+#I finished this problem in 35 minutes.
