3e1ccf13ed67542fa9ec697caa4292ef7a5e4a71
Ethan <ethantam@berkeley.edu>
2023-04-30 01:37:33 -0700

    finished #9

---
+++ Assignment2/DisconnectCycle.java
@@ -0,0 +1,22 @@
+// basic iteration
+// Time Complexity: N
+// Space Complexity: 1
+
+public class DisconnectCycle extends SinglyLinkedList {
+    
+    public DisconnectCycle() {
+        super();
+    }
+
+    public void disconnectCyc() {
+        // get to last Node, since that's where cycles can only exist in singly linked lists
+        Node p = this.head.next;
+        for (int i = 0; i < this.size - 1; i++) {
+            p = p.next;
+        }
+
+        // the 'next' of the last Node of the linked list should always be null
+        // thus eliminates any possible cycles
+        p.next = null;
+    }
+}
