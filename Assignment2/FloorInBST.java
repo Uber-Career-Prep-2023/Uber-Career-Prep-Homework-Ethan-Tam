ec28234aabc201f5388bfa5d7a76acc4e3978559
Ethan <ethantam@berkeley.edu>
2023-05-01 12:19:19 -0700

    finished #11

---
+++ Assignment2/FloorInBST.java
@@ -0,0 +1,28 @@
+// BST Search
+// Time Complexity: logN
+// Space Complexity: 1
+
+public class FloorInBST extends BinarySearchTree {
+    
+    public FloorInBST() {
+        super();
+    }
+
+    public int getFloor(int target) {
+        Node prev = null;
+        Node curr = this.root;
+
+        while (curr != null) {
+            if (curr.num == target) {
+                return curr.num;
+            } else if (curr.num < target) {
+                prev = curr;
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+
+        return prev.num;
+    }
+}
