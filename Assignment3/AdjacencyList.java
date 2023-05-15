cb800e7de2faafd8feef4ec1a94bf50aa78d850e
Ethan <ethantam@berkeley.edu>
2023-05-15 11:09:52 -0700

    finished dfs

--- Assignment3/AdjacencyList.java
+++ Assignment3/AdjacencyList.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 import java.util.Queue;
 import java.util.Stack;
@@ -15,18 +16,48 @@ public class AdjacencyList {
     }
 
     public static void main(String[] args) {
-        AdjacencyList aL = new AdjacencyList();
-        aL.edges.add(new Pair(1,2));
-        aL.edges.add(new Pair(2,3));
-        aL.edges.add(new Pair(1,3));
-        aL.edges.add(new Pair(3,2));
-        aL.edges.add(new Pair(2,0));
 
-        Map<Integer, Set<Integer>> m = aL.adjacencySet(aL.edges);
+        System.out.println("adjacency list test case");
+        // adjacency list test case
+        AdjacencyList a = new AdjacencyList();
+        a.edges.add(new Pair(1,2));
+        a.edges.add(new Pair(2,3));
+        a.edges.add(new Pair(1,3));
+        a.edges.add(new Pair(3,2));
+        a.edges.add(new Pair(2,0));
+
+        Map<Integer, Set<Integer>> m = a.adjacencySet(a.edges);
 
         for (int i : m.keySet()) {
             System.out.println(i + " : " + m.get(i).toString());
         }
+
+        AdjacencyList b = new AdjacencyList();
+        b.edges.add(new Pair(0,1));
+        b.edges.add(new Pair(0,3));
+        b.edges.add(new Pair(1,0));
+        b.edges.add(new Pair(1,2));
+        b.edges.add(new Pair(2,1));
+        b.edges.add(new Pair(2,3));
+        b.edges.add(new Pair(3,0));
+        b.edges.add(new Pair(3,2));
+
+        Map<Integer, Set<Integer>> m1 = b.adjacencySet(b.edges);
+        for (int i : m1.keySet()) {
+            System.out.println(i + " : " + m1.get(i).toString());
+        }
+
+        // bfs test case
+        System.out.println("bfs test case");
+        System.out.println(b.bfs(2, m1));
+        System.out.println(b.bfs(4, m1));
+
+        // dfs test case
+        System.out.println("dfs test case");
+        System.out.println(b.dfs(2, m1));
+        System.out.println(b.dfs(4, m1));
+
+
         
     }
 
@@ -77,14 +108,53 @@ public class AdjacencyList {
     }
 
     // search for target node using breadth-first-search
-    // return true if target node exists, false if no
+    // return true if target node exists, false if not
     public boolean bfs(int target, Map<Integer, Set<Integer>> graph) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        ArrayList<Integer> visited = new ArrayList<>();
+        int firstKey = (Integer) graph.keySet().toArray()[0];
+
+        q.add(firstKey);
+
+        while (q.size() > 0) {
+            int popFirst = q.poll();
+            if (popFirst == target) {
+                return true;
+            }
+            visited.add(popFirst);
+            Set<Integer> s = graph.get(popFirst);
+            for (int i : s) {
+                if (!visited.contains(i)) {
+                    q.add(i);
+                }
+            }
+        }
         return false;
     }
 
     // search for target node using depth-first-search
-    // return true if target node exists, false if no
+    // return true if target node exists, false if not
     public boolean dfs(int target, Map<Integer, Set<Integer>> graph) {
+        Stack<Integer> s = new Stack<>();
+        ArrayList<Integer> visited = new ArrayList<>();
+        int firstKey = (Integer) graph.keySet().toArray()[0];
+        s.push(firstKey);
+
+        while (s.size() > 0) {
+            int popFirst = s.pop();
+            if (popFirst == target) {
+                return true;
+            }
+            visited.add(popFirst);
+            Set<Integer> val = graph.get(popFirst);
+            for (int i : val) {
+                if (!visited.contains(i)) {
+                    s.push(i);
+                }
+            }
+        }
+
+
         return false;
     }
 
